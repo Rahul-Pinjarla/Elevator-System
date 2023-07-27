d06cd716bf3d126ffcfbb72a91a93bbd2f568403
RahulPGS <rahul@mrkt365.com>
2023-07-28 05:19:52 +0530

    [add] models

--- elevator_app/admin.py
+++ elevator_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import ElevatorSystem, ElevatorRequest, ElevatorStation
 
 # Register your models here.
+admin.site.register(ElevatorSystem)
+admin.site.register(ElevatorRequest)
+admin.site.register(ElevatorStation)
--- elevator_app/models.py
+++ elevator_app/models.py
@@ -1,4 +1,64 @@
 from django.db import models
+import enum
 
-# Create your models here.
 
+@enum.unique
+class Directions(str, enum.Enum):
+    UP = "UP"
+    DOWN = "DOWN"
+
+    @classmethod
+    def choices(cls):
+        return [(item.value, item.name) for item in cls]
+
+
+class TimeStampBaseModel(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class ElevatorSystem(TimeStampBaseModel):
+    """
+    Every time an elevator system is initialised it's record will be added to this Model
+    """
+
+    stations_count = models.PositiveIntegerField()
+    curr_station = models.ForeignKey(
+        "ElevatorStation", on_delete=models.DO_NOTHING, null=True, blank=True
+    )
+    curr_direction = models.CharField(
+        max_length=50, choices=Directions.choices(), default=Directions.UP
+    )
+    building_name = models.CharField(max_length=100, default="Name Unkown")
+
+    def get_pending_requests(self):
+        return ElevatorRequest.objects.filter(elevator_system=self, served_on=None)
+
+
+class ElevatorStation(TimeStampBaseModel):
+    elevator_system = models.ForeignKey(ElevatorSystem, on_delete=models.CASCADE)
+    floor_no = models.PositiveIntegerField()
+    under_maintenance_since = models.DateTimeField(default=None, blank=True, null=True)
+
+
+class ElevatorRequest(TimeStampBaseModel):
+    """
+    Every time an elevator get's a request it will be recorded here
+    """
+
+    elevator_system = models.ForeignKey(ElevatorSystem, on_delete=models.CASCADE)
+    from_station = models.ForeignKey(
+        ElevatorStation, on_delete=models.CASCADE, related_name="from_station"
+    )
+    to_station = models.ForeignKey(
+        ElevatorStation,
+        on_delete=models.CASCADE,
+        related_name="to_station",
+        null=True,
+        blank=True,
+    )
+    served_on = models.DateTimeField(blank=True, null=True, default=None)
+    skip_count = models.PositiveIntegerField(default=0)
